538aaf47a9829f83909fd8fffd886b2674053eb4
malek <pocmalek@gmail.com>
2022-11-10 23:32:54 -0800

    got rotation working for text

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod parser;
 mod flashcard;
 
-use std::ops::Add;
+use std::ops::{Add, Mul, Neg};
 use glam::{Mat4, Quat, Vec3, vec3};
 use prisma::{Rgb, Rgba};
 use stereokit::material::{DEFAULT_ID_MATERIAL, Material};
@@ -9,7 +9,7 @@ use stereokit::mesh::Mesh;
 use stereokit::model::Model;
 use stereokit::render::RenderLayer;
 use stereokit::{high_level, pose, Settings, StereoKit, ui};
-use stereokit::high_level::math_traits::{PosTrait, RotationTrait, ScaleTrait};
+use stereokit::high_level::math_traits::{MatrixTrait, PosTrait, RotationTrait, ScaleTrait};
 
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "my-tag")))]
 pub fn main() {
@@ -18,6 +18,7 @@ pub fn main() {
 }
 use anyhow::{Context, Result};
 use mint::{Quaternion, Vector3};
+use stereokit::high_level::quat_from_angles;
 use stereokit::input::Handed;
 use stereokit::input::Handed::Right;
 use stereokit::lifecycle::DrawContext;
@@ -44,70 +45,74 @@ pub fn my_func() -> Result<()> {
     let mut my_pos = Pose::new(Vec3::new(0f32, 0f32, 30f32).into(), Quat::IDENTITY.into());
 
     let mut flash_card = high_level::model::Model::from_mesh(&stereokit, &cube_mesh, &cube_material)?;
-
-
-    flash_card.translate(0.0, 0.0, -0.7);
-    flash_card.set_scale(1.0, 1.0, 0.1);
     flash_card.tint.set_color(Rgb::new(0.5, 0.5, 0.5));
-
     let mut answer_text = high_level::text::Text::new(&stereokit, rand_cards.unwrap().0.term.0, [0.0, 0.0, -0.6], [0.0, 180.0, 0.0], [1.0, 1.0, 1.0]);
+
+    //flash_card.translate(0.0, 0.0, -0.7);
+    //flash_card.set_scale(1.0, 1.0, 0.1);
+    //flash_card.tint.set_color(Rgb::new(0.5, 0.5, 0.5));
+
+    let mut flash_card = FlashCard {
+        model: flash_card,
+        text: answer_text,
+    };
+    flash_card.set_pos(0.0, 0.0, -3.0);
+    flash_card.set_rotation(0.0, 30.0, 0.0);
+    let mut rotation_value = 0.0;
     stereokit.run(|sk, ctx| {
+        //flash_card.draw(ctx);
+        //answer_text.draw_at(ctx);
         flash_card.draw(ctx);
-        answer_text.draw_at(ctx);
+        flash_card.rotate(0.0, 0.6, 0.0);
+        flash_card.translate(0.0, 0.0, 0.005);
+        //flash_card.set_rotation(0.0, rotation_value, 0.0);
+        rotation_value += 0.1;
 
     }, |_| {});
     Ok(())
 }
 
-// struct FlashCardCube {
-//     position: Vec3,
-//     rotation: Quat,
-//     text_rotation: Quat,
-//     cube_model: Model,
-//     tint: Rgba<f32>,
-//     text: String,
-//     visible: bool,
-// }
-// impl FlashCardCube {
-//     pub fn new(sk: &mut StereoKit) -> Option<Self> {
-//         let cube_mesh = Mesh::gen_cube(&sk, Vec3::new(0.05, 1.0, 1.0).into(), 1)?;
-//         let cube_material = Material::copy_from_id(&sk, DEFAULT_ID_MATERIAL)?;
-//         Some(FlashCardCube {
-//             position: Default::default(),
-//             cube_model: Model::from_mesh(&sk, &cube_mesh, &cube_material)?,
-//             tint: Rgba::new(Rgb::new(0.6_f32, 0.5_f32, 0.5_f32), 1_f32),
-//             text: "test_text".to_string(),
-//             rotation: Quat::from_rotation_y(55f32),
-//             text_rotation: Quat::from_rotation_y(135f32),
-//             visible: true,
-//         })
-//     }
-//     pub fn draw(&self, sk: &mut StereoKit, ctx: &DrawContext) {
-//         let pos = Mat4::from_scale_rotation_translation(Vec3::new(1f32, 1f32, 1f32), self.rotation.into(), self.position).into();
-//         let temp_pos = self.position.add(Vec3::new(00f32, 0f32, 0.2f32));
-//         let text_pos = Mat4::from_scale_rotation_translation(Vec3::new(1f32, 1f32, 1f32), self.text_rotation.into(), temp_pos);
-//         self.cube_model.draw(ctx, pos, self.tint, RenderLayer::Layer0);
-//         stereokit::text::draw_at(&ctx,
-//                                  &self.text.as_str(),
-//                                  text_pos,
-//                                  &TextStyle::default(sk),
-//                                  TextAlign::Center,
-//                                  TextAlign::Center,
-//                                  Vec3::new(0f32, 0f32, 0f32),
-//                                  Rgba::new(Rgb::new(1_f32, 1_f32, 1_f32), 1_f32).into());
-//     }
-//     pub fn contains(&self, sk: &StereoKit, point: Vec3) -> bool {
-//         let pos = Mat4::from_scale_rotation_translation(Vec3::new(1f32, 1f32, 1f32), self.rotation.into(), self.position);
-//         let mut matrix = SKMatrix::new(pos.into());
-//         self.cube_model.get_bounds(sk).bounds_point_contains(matrix.transform_point(point.into()))
-//     }
-// }
-// saber_model.draw(
-//     drw_ctx,
-//     Mat4::from_scale_rotation_translation(
-//         vec3(0.01, 2.0, 0.01),
-//         right_hand.palm.orientation.into(),
-//         Vec3::from(right_hand.palm.position).add(Vec3::new(0.0, 0.9, 0.0)),
-//     ).into(),
-//     Rgba::new(Rgb::new(1_f32, 1_f32, 1_f32), 1_f32).into(),
-//     RenderLayer::Layer0);
+struct FlashCard {
+    pub model: high_level::model::Model,
+    pub text: high_level::text::Text,
+}
+impl FlashCard {
+    pub fn draw(&self, ctx: &DrawContext) {
+        self.model.draw(ctx);
+        self.text.draw_at(ctx);
+    }
+    fn sync_model_text_matrix(&mut self) {
+        let new_matrix = self.model.get_matrix().mul_mat4(&Mat4::from_translation(Vec3::new(0.0, 0.0, -0.5001)));
+        self.text.set_matrix(new_matrix);
+    }
+}
+
+impl PosTrait for FlashCard {
+    fn get_pos_vec(&self) -> Vec3 {
+        self.model.get_pos_vec()
+    }
+
+    fn set_pos_vec(&mut self, pos: impl Into<stereokit::values::Vec3>) {
+        self.model.set_pos_vec(pos.into().clone());
+        self.sync_model_text_matrix();
+    }
+
+    fn translate_vec(&mut self, translation: impl Into<stereokit::values::Vec3>) {
+        self.set_pos_vec(glam::Vec3::from(translation.into()).add(self.get_pos_vec()));
+    }
+}
+impl RotationTrait for FlashCard {
+    fn get_rotation_vec(&self) -> Vec3 {
+        self.model.get_rotation_vec()
+    }
+
+    fn set_rotation_vec(&mut self, rotation: impl Into<stereokit::values::Vec3>) {
+        let rotation = rotation.into();
+        self.model.set_rotation_vec(rotation.clone());
+        self.sync_model_text_matrix();
+    }
+
+    fn rotate_vec(&mut self, rotation: impl Into<stereokit::values::Vec3>) {
+        self.set_rotation_vec(glam::Vec3::from(rotation.into()).add(self.get_rotation_vec()))
+    }
+}
